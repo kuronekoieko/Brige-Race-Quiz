1b33a0e0af777c625ecfbffb12e8dd25de99b62e
家子智成 <>
2021-05-08 14:20:55 +0900

    スクリプト仮作成

---
+++ Assets/0_MyAssets/Scripts/Game/Character.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] bool isPlayer;
+    public Player player;
+    public NPC nPC;
+
+
+    private void Awake()
+    {
+        if (isPlayer)
+        {
+            DestroyImmediate(nPC);
+        }
+        else
+        {
+            DestroyImmediate(player);
+        }
+    }
+
+
+    void Start()
+    {
+        if (player) player.OnStart();
+        if (nPC) nPC.OnStart();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    public void Walk(Vector3 direction)
+    {
+        var vel = rb.velocity;
+        vel.x = direction.normalized.x;
+        vel.z = direction.normalized.z;
+        rb.velocity = vel;
+    }
+}
---
+++ Assets/0_MyAssets/Scripts/Game/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public void OnStart()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
